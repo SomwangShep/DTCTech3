84965a7b591bdfa8a61b38587eb5d0630e95c778
Somwang <somwang@gmail.com>
2018-02-09 13:20:25 +0000

    Complete-218 Payment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180209014235) do
+ActiveRecord::Schema.define(version: 20180209131533) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 20180209014235) do
   create_table "messages", force: :cascade do |t|
     t.text     "content"
     t.integer  "chef_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.string   "email"
+    t.string   "token"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
