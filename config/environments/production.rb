155487d8bfb7326d4cfe7b25bf0176e3ba5c27bd
Somwang <somwang@gmail.com>
2018-02-09 00:15:23 +0000

    Update Redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.web_socket_server_url = "wss://dtctech.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://dtctech.herokuapp.com', 'http://dtctech.herokuapp.com/']
+  config.web_socket_server_url = "wss://dtctech5.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://dtctech5.herokuapp.com', 'http://dtctech5.herokuapp.com/']
 end
